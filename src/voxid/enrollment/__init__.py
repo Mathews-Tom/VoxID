7c35550e64d9a6c9f2d51937ba1230df8fdb7bcb
Mathews-Tom <tommathews2007@gmail.com>
2026-03-23 21:01:09 +0530

    build(enrollment): add pyloudnorm dependency and export quality modules
    Add pyloudnorm for ITU-R BS.1770-4 loudness normalization. Add mypy
    override for untyped pyloudnorm stubs. Export QualityConfig,
    QualityGate, QualityReport, AudioPreprocessor from enrollment package.

--- src/voxid/enrollment/__init__.py
+++ src/voxid/enrollment/__init__.py
@@ -7,14 +7,21 @@ from .phoneme_tracker import (
     load_cmudict,
     text_to_phonemes,
 )
+from .preprocessor import AudioPreprocessor
+from .quality_gate import QualityConfig, QualityGate, QualityReport, estimate_snr
 from .script_generator import EnrollmentPrompt, ScriptGenerator
 
 __all__ = [
     "ALL_PHONEMES",
+    "AudioPreprocessor",
     "EnrollmentPrompt",
     "PHONEME_WEIGHTS",
     "PhonemeTracker",
+    "QualityConfig",
+    "QualityGate",
+    "QualityReport",
     "ScriptGenerator",
+    "estimate_snr",
     "load_cmudict",
     "text_to_phonemes",
 ]
